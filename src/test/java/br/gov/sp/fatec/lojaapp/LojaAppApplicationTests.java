038179948a6fbc96b85ce6d55f9c09b32eb02d23
jujalu <jujalu@gmail.com>
2021-08-27 14:58:05 -0300

    commit inicial

---
+++ src/test/java/br/gov/sp/fatec/lojaapp/LojaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.lojaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
